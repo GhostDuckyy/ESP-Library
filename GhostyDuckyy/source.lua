6dcdeab5382f102433ca0ea3039e1ff0ad29b276
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-10-15 22:08:43 +0800

    Update source.lua

--- GhostyDuckyy/source.lua
+++ GhostyDuckyy/source.lua
@@ -8,6 +8,7 @@ local CurrentCamera = workspace.CurrentCamera
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
+local Mouse = LocalPlayer:GetMouse()
 
 function Render:RenderObject(t, p)
     local Drawing_type = tostring(t):lower()
@@ -133,7 +134,6 @@ function Render:RenderObject(t, p)
                         end
                     end
                 elseif v.Object and property.Position then
-                    print("dick")
                     for x,y in next, Options do
                         v.Label[x] = y
                     end
@@ -141,7 +141,7 @@ function Render:RenderObject(t, p)
             end
         end)
 
-        func = {
+        local func = {
             DestoryRender = function()
                 DescendantAdded:Disconnect()
                 RenderStepped:Disconnect()
@@ -228,8 +228,218 @@ function Render:RenderObject(t, p)
         }
 
         return func
-    end
 
+    elseif Drawing_type == "line" or Drawing_type:find("trace") then
+        local ObjectName = property.Name
+        local ClassName = property.ClassName
+        local LineForm = tostring(property.Form) or tostring(property.LineForm) or "mouse"
+        local FilterObject = property.Filter or property.FilterObject or false
+
+        local Options = property.Options or {
+            Visible = true,
+            Transparency = 1,
+            Color = Color3.new(1,1,1),
+            Thickness = 1,
+        }
+        local Folder = {}
+
+        function addLine(instance)
+            if tostring(instance.Name):lower() == tostring(ObjectName):lower() and instance:IsA(tostring(ClassName)) or tostring(instance.ClassName):lower() == tostring(ClassName):lower() then
+                if FilterObject ~= false then
+                    for index,Table in next, FilterObject do
+                        if Table.Name and Table.ClassName then
+                            for i,v in ipairs(instance:GetDescendants()) do
+                                if tostring(v.ClassName):lower() == tostring(Table.ClassName):lower() and tostring(v.Name):lower() == tostring(Table.Name):lower() then
+                                    local UwU = {Line = Drawing.new("Line"), Object = instance}
+                                    table.insert(Folder, UwU)
+                                end
+                            end
+                        elseif Table.ClassName then
+                            for i,v in ipairs(instance:GetDescendants()) do
+                                if tostring(v.ClassName):lower() == tostring(Table.ClassName):lower() then
+                                    local UwU = {Line = Drawing.new("Line"), Object = instance}
+                                    table.insert(Folder, UwU)
+                                end
+                            end
+                        elseif Table.Name then
+                            for i,v in ipairs(instance:GetDescendants()) do
+                                if tostring(v.Name):lower() == tostring(Table.Name):lower() then
+                                    local UwU = {Line = Drawing.new("Line"), Object = instance}
+                                    table.insert(Folder, UwU)
+                                end
+                            end
+                        else
+                            local UwU = {Line = Drawing.new("Line"), Object = instance}
+                            table.insert(Folder, UwU)
+                        end
+                    end
+                else
+                    local UwU = {Line = Drawing.new("Line"), Object = instance}
+                    table.insert(Folder, UwU)
+                end
+            end
+        end
+
+        for i,v in ipairs(workspace:GetDescendants()) do
+            addLine(v)
+        end
+
+        local DescendantAdded = workspace.DescendantAdded:Connect(function(v)
+            task.wait(.1)
+            addLine(v)
+        end)
+        local RenderStepped = RunService.RenderStepped:Connect(function()
+            for i,v in next, Folder do
+                if Render.AutoRemove then
+                    if v.Object == nil then
+                        v.Line.Visible = false
+                        v.Line:Remove()
+                        table.remove(Folder,i)
+                    end
+                end
+
+                if v.Object and not property.To then
+                    local Vector, Screen = CurrentCamera:WorldToViewportPoint(v.Object.Position)
+                    if Render.CheckIfOnScreen then
+                        if Screen then
+                            v.Line.To = Vector2.new(Vector.X, Vector.Y)
+
+                            if LineForm:lower() == "mouse" then
+                                v.Line.Form = Vector2.new(Mouse.X, Mouse.Y)
+                            elseif LineForm:lower() == "top" then
+                                
+                            elseif LineForm:lower() == "center" then
+                                
+                            elseif LineForm:lower() == "bottom" then
+                                
+                            end
+
+                            for x,y in next, Options do
+                                v.Line[x] = y
+                            end
+                        else
+                            v.Line.Visible = false
+                        end
+                    else
+                        v.Line.To = Vector2.new(Vector.X, Vector.Y)
+
+                        if LineForm:lower() == "mouse" then
+                            v.Line.Form = Vector2.new(Mouse.X, Mouse.Y)
+                        elseif LineForm:lower() == "top" then
+                            
+                        elseif LineForm:lower() == "center" then
+                            
+                        elseif LineForm:lower() == "bottom" then
+                            
+                        end
+
+                        for x,y in next, Options do
+                            v.Line[x] = y
+                        end
+                    end
+                end
+            end
+        end)
+
+        local func = {
+            DestoryRender = function()
+                DescendantAdded:Disconnect()
+                RenderStepped:Disconnect()
+
+                for i,v in next, Folder do
+                    v.Label.Visible = false
+                    v.Label:Remove()
+                end
+
+                task.wait(.5)
+                table.clear(Folder)
+            end,
+            SetOptions = function(prop, value)
+                for index,v in next, Options do
+                    if tostring(index):lower() == tostring(prop):lower() then
+                        Options[index] = value
+                    end
+                end
+            end,
+            AddFilter = function(arg)
+                if not FilterObject then
+                    FilterObject = {}
+
+                    if typeof(arg) == "table" then
+                        if arg.Name and arg.ClassName then
+                            table.insert(FilterObject, arg)
+                        elseif arg.ClassName then
+                            table.insert(FilterObject, arg)
+                        elseif arg.Name then
+                            table.insert(FilterObject, arg)
+                        end
+                    else
+                        if arg.Name and arg.ClassName then
+                            table.insert(FilterObject, {arg})
+                        elseif arg.ClassName then
+                            table.insert(FilterObject, {arg})
+                        elseif arg.Name then
+                            table.insert(FilterObject, {arg})
+                        end
+                    end
+                else
+                    if typeof(arg) == "table" then
+                        if arg.Name and arg.ClassName then
+                            table.insert(FilterObject, arg)
+                        elseif arg.ClassName then
+                            table.insert(FilterObject, arg)
+                        elseif arg.Name then
+                            table.insert(FilterObject, arg)
+                        end
+                    else
+                        if arg.Name and arg.ClassName then
+                            table.insert(FilterObject, {arg})
+                        elseif arg.ClassName then
+                            table.insert(FilterObject, {arg})
+                        elseif arg.Name then
+                            table.insert(FilterObject, {arg})
+                        end
+                    end
+                end
+            end,
+            RemoveFilter = function(arg)
+                if FilterObject ~= false then
+                    for i,v in next, FilterObject do
+                        if arg.Name and arg.ClassName then
+                            if tostring(v.ClassName):lower() == tostring(arg.ClassName):lower() and tostring(v.Name):lower() == tostring(arg.Name):lower() then
+                                table.remove(FilterObject,i)
+                            end
+                        elseif arg.ClassName then
+                            if tostring(v.ClassName):lower() == tostring(arg.ClassName):lower() then
+                                table.remove(FilterObject,i)
+                            end
+                        elseif arg.Name then
+                            if tostring(v.Name):lower() == tostring(arg.Name):lower() then
+                                table.remove(FilterObject,i)
+                            end
+                        end
+                    end
+                end
+            end,
+            ClearFilter = function()
+                table.clear(FilterObject)
+                FilterObject = {}
+            end,
+        }
+
+        return func
+
+    elseif Drawing_type == "image" then
+
+    elseif Drawing_type == "circle" then
+
+    elseif Drawing_type == "square" then
+
+    elseif Drawing_type == "quad" then
+
+    elseif Drawing_type == "triangle" then
+
+    end
 end
 
 return Render
